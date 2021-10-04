258c165612dd9a56957cfd8e587343b6db7e23e8
HaiLHopital <astrovadim@gmail.com>
2021-10-04 21:41:03 +0300

    setting up everything

--- angularWebGL/src/app/app.module.ts
+++ angularWebGL/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CanvasGLComponent } from './canvas-gl/canvas-gl.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CanvasGLComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- angularWebGL/src/app/canvas-gl/canvas-gl.component.ts
+++ angularWebGL/src/app/canvas-gl/canvas-gl.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WebGLService } from '../services/web-gl.service';
 
 @Component({
   selector: 'app-canvas-gl',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CanvasGLComponent implements OnInit {
 
-  constructor() { }
+  constructor(private webglService: WebGLService) { }
 
   ngOnInit(): void {
   }
