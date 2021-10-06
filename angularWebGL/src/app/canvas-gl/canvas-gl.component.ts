e405d31907c2a2c92fc38865636e9a1ae130c498
HaiLHopital <astrovadim@gmail.com>
2021-10-06 22:08:13 +0300

    init context

--- angularWebGL/src/app/canvas-gl/canvas-gl.component.ts
+++ angularWebGL/src/app/canvas-gl/canvas-gl.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { WebGLService } from '../services/web-gl.service';
 
 @Component({
   selector: 'app-canvas-gl',
   templateUrl: './canvas-gl.component.html',
-  styleUrls: ['./canvas-gl.component.scss']
+  styleUrls: ['./canvas-gl.component.scss'],
 })
-export class CanvasGLComponent implements OnInit {
+export class CanvasGLComponent implements OnInit, AfterViewInit {
+  @ViewChild('sceneCanvas') private canvas: ElementRef<HTMLCanvasElement>;;
 
-  constructor(private webglService: WebGLService) { }
+  constructor(private webglService: WebGLService) {}
 
   ngOnInit(): void {
+    console.log('oninit');
   }
 
+  ngAfterViewInit(): void {
+    console.log('afterinit');
+    if (!this.canvas) {
+      alert('canvas not supplied! cannot bind WebGL context!');
+      return;
+    }
+    console.log(this.canvas)
+    this.webglService.initializeWebGLContext(this.canvas.nativeElement)
+  }
 }
--- angularWebGL/src/app/services/web-gl.service.ts
+++ angularWebGL/src/app/services/web-gl.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WebGLService {
+  private _renderingContext: RenderingContext;
+  private get gl(): WebGLRenderingContext {
+    return <WebGLRenderingContext>this._renderingContext;
+  }
 
-  constructor() { }
+  constructor() {}
+
+  initializeWebGLContext(canvas: HTMLCanvasElement): void {
+    console.log(canvas.getAttribute('style'))
+    this._renderingContext =
+      canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+    if (!this.gl) {
+      alert('Unable to initialize WebGL. Your browser may not support it.');
+      return;
+    }
+    this.setWebGLCanvasDimenviosns(canvas);
+    this.iniliseWebGLCanvas();
+  }
+
+  setWebGLCanvasDimenviosns(canvas: HTMLCanvasElement) {
+    this.gl.canvas.width = canvas.clientWidth;
+    this.gl.canvas.height = canvas.clientHeight;
+  }
+
+  iniliseWebGLCanvas() {
+    this.gl.clearColor(0,0,0,1)
+
+    this.gl.enable(this.gl.DEPTH_TEST);
+
+    this.gl.depthFunc(this.gl.LEQUAL);
+
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+
+  }
 }
