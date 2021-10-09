d109a6be2638e99901e8d24fa1b2598a6aca200b
HaiLHopital <astrovadim@gmail.com>
2021-10-09 21:32:05 +0300

    first square

--- angularWebGL/src/app/canvas-gl/canvas-gl.component.ts
+++ angularWebGL/src/app/canvas-gl/canvas-gl.component.ts
@@ -6,6 +6,8 @@ import {
   ViewChild,
 } from '@angular/core';
 import { WebGLService } from '../services/web-gl.service';
+import { interval } from 'rxjs';
+
 
 @Component({
   selector: 'app-canvas-gl',
@@ -14,6 +16,8 @@ import { WebGLService } from '../services/web-gl.service';
 })
 export class CanvasGLComponent implements OnInit, AfterViewInit {
   @ViewChild('sceneCanvas') private canvas: ElementRef<HTMLCanvasElement>;;
+  private _60fpsInterval = 16.666666666666666667;
+  private gl: WebGLRenderingContext
 
   constructor(private webglService: WebGLService) {}
 
@@ -28,6 +32,21 @@ export class CanvasGLComponent implements OnInit, AfterViewInit {
       return;
     }
     console.log(this.canvas)
-    this.webglService.initializeWebGLContext(this.canvas.nativeElement)
+    this.gl = this.webglService.initializeWebGLContext(this.canvas.nativeElement);
+    this.drawScene();
+   /* const drawSceneInterval = interval(this._60fpsInterval);
+    drawSceneInterval.subscribe(() => {
+      
+    });*/
+  }
+  private drawScene() {
+    // draw the scene
+    const offset = 0;
+    const vertexCount = 4;
+    this.gl.drawArrays(
+      this.gl.TRIANGLE_STRIP,
+      offset,
+      vertexCount
+    );
   }
 }
